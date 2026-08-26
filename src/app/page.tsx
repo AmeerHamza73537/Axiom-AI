27d53241247c608c3873e18473e0466a1304c485
AmeerHamza73537 <zarb55091@gmail.com>
2026-08-26 18:00:11 +0500

    Add TRPC integration and update dependencies
    - Introduced TRPC for API handling with new routes and client setup.
    - Updated package.json and package-lock.json with new dependencies.
    - Created query client and context for TRPC.
    - Implemented hydration and suspense for data fetching in components.
    - Added TypeScript settings for improved development experience.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
-import { Button } from "@base-ui/react"
 
-const Page = () => {
+import { useTRPC } from "@/trpc/client"
+import { caller, getQueryClient, trpc } from "@/trpc/server"
+import { dehydrate, HydrationBoundary, useQuery } from "@tanstack/react-query"
+import { Client } from "./client"
+import { Suspense } from "react"
+const Page = async () => {
+  // const trpc = useTRPC()
+  // const data  = await caller.hello({text: "from the client!"})
+  const queryClient = getQueryClient()
+  void queryClient.prefetchQuery(trpc.hello.queryOptions({text: "from the client!"}))
   return(
-    <div>
-      Hello World!
-      <Button>Helloooooo</Button>
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      {/* <div> */}
+        <Suspense fallback={<div>Loading...</div>}>
+          <Client/>
+        </Suspense>
+        
+        {/* {JSON.stringify(data)} */}
+        {/* <Button>Helloooooo</Button> */}
+      {/* </div> */}
+    </HydrationBoundary>
   )
 }
 export default Page
