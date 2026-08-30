146575a24c40cf977e8a4898503cdd8deeaa5ad1
AmeerHamza73537 <zarb55091@gmail.com>
2026-08-30 21:57:40 +0500

    feat: integrate Inngest for background job processing
    - Added Inngest as a dependency in package.json.
    - Removed the Client component and related code from page.tsx.
    - Implemented a new mutation in the TRPC router to invoke background jobs using Inngest.
    - Created a new API route for Inngest to handle incoming requests.
    - Set up Inngest client and defined a function to process tasks.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,45 @@
+'use client'
 
 import { useTRPC } from "@/trpc/client"
-import { caller, getQueryClient, trpc } from "@/trpc/server"
-import { dehydrate, HydrationBoundary, useQuery } from "@tanstack/react-query"
-import { Client } from "./client"
-import { Suspense } from "react"
-const Page = async () => {
-  // const trpc = useTRPC()
-  // const data  = await caller.hello({text: "from the client!"})
-  const queryClient = getQueryClient()
-  void queryClient.prefetchQuery(trpc.hello.queryOptions({text: "from the client!"}))
-  return(
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      {/* <div> */}
-        <Suspense fallback={<div>Loading...</div>}>
-          <Client/>
-        </Suspense>
+import { Button } from "@base-ui/react"
+import { useMutation } from "@tanstack/react-query"
+
+// import { useTRPC } from "@/trpc/client"
+// import { caller, getQueryClient, trpc } from "@/trpc/server"
+// import { dehydrate, HydrationBoundary, useQuery } from "@tanstack/react-query"
+// import { Client } from "./client"
+// import { Suspense } from "react"
+// const Page = async () => {
+//   // const trpc = useTRPC()
+//   // const data  = await caller.hello({text: "from the client!"})
+//   const queryClient = getQueryClient()
+//   void queryClient.prefetchQuery(trpc.hello.queryOptions({text: "from the client!"}))
+//   return(
+//     <HydrationBoundary state={dehydrate(queryClient)}>
+//       {/* <div> */}
+//         <Suspense fallback={<div>Loading...</div>}>
+//           <Client/>
+//         </Suspense>
         
-        {/* {JSON.stringify(data)} */}
-        {/* <Button>Helloooooo</Button> */}
-      {/* </div> */}
-    </HydrationBoundary>
+//         {/* {JSON.stringify(data)} */}
+//         {/* <Button>Helloooooo</Button> */}
+//       {/* </div> */}
+//     </HydrationBoundary>
+//   )
+// }
+// export default Page
+
+
+const Page = () => {
+  const trpc = useTRPC()
+  const invoke = useMutation(trpc.invoke.mutationOptions({}))
+  
+  return (
+    <div>
+      <Button onClick={()=> invoke.mutate({text: "John"})}>
+        Invoke Background Job
+      </Button>
+    </div>
   )
 }
 export default Page
